924fe1ea913e775a060bbc32754ac5d169df765a
Nickolas2204 <nickolasandrew2204@gmail.com>
2025-01-07 17:21:00 -0300

    jogonumerosecreto

--- desafio1.js
+++ desafio1.js
@@ -2,4 +2,4 @@ let listaGenerica = []
 let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
 linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
 nomes = ['JavaScript', 'Python', 'Go'];
-console.log(nome[0]);
+console.log(nome[2]);
